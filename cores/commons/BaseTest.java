099af3f164e1451d11e3d7de8753b29035783b48
Ngoc Quach <Ngoc Quach@DESKTOP-NGOC>
2022-04-26 17:26:42 +0700

    update multi browsers

---
+++ cores/commons/BaseTest.java
@@ -0,0 +1,40 @@
+package commons;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	private WebDriver driver;
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().arch64().setup();
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserName.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equals("IE")) {
+			WebDriverManager.iedriver().arch32().setup();
+			driver = new InternetExplorerDriver();
+		} else if (browserName.equals("Opera")) {
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+		} else {
+			throw new RuntimeException("Invalid browser name");
+		}
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		return driver;
+	}
+}
--- testcases/com/nopcommerce/user/Level_04_Multiple_Browsers.java
+++ testcases/com/nopcommerce/user/Level_04_Multiple_Browsers.java
@@ -96,69 +96,6 @@ public class Level_04_Multiple_Browsers extends BaseTest{
 		registerPage.clickOnLogoutLink();
 	}
 
-	@Test
-	public void Register_04_Existing_Email() {
-		System.out.println("Register_04 - Step 01: Click on Register link");
-		homePage.clickOnRegisterLink();
-
-		registerPage = new RegisterPageObject(driver);
-
-		System.out.println("Register_04 - Step 02: Input into required fields");
-		registerPage.inputIntoFirstNameTextbox(firstName);
-		registerPage.inputIntoLastNameTextbox(lastName);
-		registerPage.inputIntoEmailTextbox(emailAddress);
-		registerPage.inputIntoPasswordTextbox(password);
-		registerPage.inputIntoConfirmPasswordTextbox(password);
-
-		System.out.println("Register_04 - Step 03: Click on Register button");
-		registerPage.clickOnRegisterButton();
-
-		System.out.println("Register_04 - Step 04: Verify error message of existing email displayed");
-		Assert.assertEquals(registerPage.getErrorMessageExistingEmail(), "The specified email already exists");
-	}
-
-	@Test
-	public void Register_05_Password_Less_Than_6_Characters() {
-		System.out.println("Register_05 - Step 01: Click on Register link");
-		homePage.clickOnRegisterLink();
-
-		registerPage = new RegisterPageObject(driver);
-
-		System.out.println("Register_05 - Step 02: Input into required fields");
-		registerPage.inputIntoFirstNameTextbox(firstName);
-		registerPage.inputIntoLastNameTextbox(lastName);
-		registerPage.inputIntoEmailTextbox(emailAddress);
-		registerPage.inputIntoPasswordTextbox("123");
-		registerPage.inputIntoConfirmPasswordTextbox("123");
-
-		System.out.println("Register_05 - Step 03: Click on Register button");
-		registerPage.clickOnRegisterButton();
-
-		System.out.println("Register_05 - Step 04: Verify error message displayed");
-		Assert.assertEquals(registerPage.getErrorMessageBelowPasswordTextbox(), "Password must meet the following rules:\nmust have at least 6 characters");
-	}
-
-	@Test
-	public void Register_06_Invalid_Confirm_Password() {
-		System.out.println("Register_06 - Step 01: Click on Register link");
-		homePage.clickOnRegisterLink();
-
-		registerPage = new RegisterPageObject(driver);
-
-		System.out.println("Register_06 - Step 02: Input into required fields");
-		registerPage.inputIntoFirstNameTextbox(firstName);
-		registerPage.inputIntoLastNameTextbox(lastName);
-		registerPage.inputIntoEmailTextbox(emailAddress);
-		registerPage.inputIntoPasswordTextbox(password);
-		registerPage.inputIntoConfirmPasswordTextbox(emailAddress);
-
-		System.out.println("Register_06 - Step 03: Click on Register button");
-		registerPage.clickOnRegisterButton();
-
-		System.out.println("Register_06 - Step 04: Verify error message displayed");
-		Assert.assertEquals(registerPage.getErrorMessageBelowConfirmPasswordTextbox(), "The password and confirmation password do not match.");
-	}
-
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
