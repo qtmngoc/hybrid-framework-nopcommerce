e903c75d5926e8f19675b4ce77473485e643b075
Ngoc Quach <Ngoc Quach@192.168.56.1>
2022-06-13 17:14:46 +0700

    Log - ReportNG

---
+++ testcases/com/nopcommerce/user/Level_14_Log_ReportNG.java
@@ -0,0 +1,96 @@
+package com.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.nopCommerce.BaseTest;
+import commons.nopCommerce.PageGeneratorManager;
+import pageObjects.nopCommerce.user.UserCustomerInfoPageObject;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserLoginPageObject;
+import pageObjects.nopCommerce.user.UserRegisterPageObject;
+
+public class Level_14_Log_ReportNG extends BaseTest {
+	
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		userHomePage = PageGeneratorManager.getUserHomePage(driver);
+
+		firstName = "Log";
+		lastName = "ReportNG";
+		email = "logreportng.level14." + generateFakeNumber() + "@mail.com";
+		password = "130622";
+	}
+
+	@Test
+	public void User_01_Register() {
+		log.info("Register - Step 01: Navigate to Register page");
+		userRegisterPage = userHomePage.clickOnRegisterLink();
+
+		log.info("Register - Step 02: Enter '" + firstName + "' into First Name text box");
+		userRegisterPage.inputIntoFirstNameTextbox(firstName);
+		
+		log.info("Register - Step 03: Enter '" + lastName + "' into Last Name text box");
+		userRegisterPage.inputIntoLastNameTextbox(lastName);
+		
+		log.info("Register - Step 04: Enter '" + email + "' into Email text box");
+		userRegisterPage.inputIntoEmailTextbox(email);
+		
+		log.info("Register - Step 05: Enter '" + password + "' into Password text box");
+		userRegisterPage.inputIntoPasswordTextbox(password);
+		
+		log.info("Register - Step 06: Enter '" + password + "' into Confirm Password text box");
+		userRegisterPage.inputIntoConfirmPasswordTextbox(password);
+		
+		log.info("Register - Step 07: Click on Register button");
+		userRegisterPage.clickOnRegisterButton();
+		
+		log.info("Register - Step 08: Verify registration success message is displayed");
+		verifyEquals(userRegisterPage.getRegisterSuccessMessage(), "Your registration completed");
+
+		log.info("Register - Step 09: Click on Logout link");
+		userHomePage = userRegisterPage.clickOnLogoutLink();
+	}
+	
+	@Test
+	public void User_02_Login() {
+		log.info("Login - Step 01: Navigate to Login page");
+		userLoginPage = userHomePage.clickOnLoginLink();
+		
+		log.info("Login - Step 02: Enter '" + email + "' into Email text box");
+		userLoginPage.inputIntoEmailTextbox(email);
+		
+		log.info("Login - Step 03: Enter '" + password + "' into Password text box");
+		userLoginPage.inputIntoPasswordTextbox(password);
+
+		log.info("Login - Step 04: Click on Login button");
+		userHomePage = userLoginPage.clickOnLoginButton();
+		
+		log.info("Login - Step 05: Verify My Account link is displayed");
+		verifyTrue(userHomePage.isMyAccountLinkDisplayed());
+		
+		log.info("Login - Step 06: Navigate to My Account page");
+		userCustomerInfoPage = userHomePage.clickOnMyAccountLink();
+		
+		log.info("Login - Step 07: Verify Customer Info page is displayed");
+		verifyFalse(userCustomerInfoPage.isCustomerInfoHeaderDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	private WebDriver driver;
+	private UserHomePageObject userHomePage;
+	private UserRegisterPageObject userRegisterPage;
+	private UserLoginPageObject userLoginPage;
+	private UserCustomerInfoPageObject userCustomerInfoPage;
+	private String firstName, lastName, email, password;
+	
+}
